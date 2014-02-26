da73edaf20d63c84fd3ce4964646c7288e1b9a2b
Conor Brady <conorjbrady1@gmail.com>
2014-02-26 11:54:14 +0000

    Getting nowhere fast

--- a.c
+++ a.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
 #include <stdio.h>
 #include <time.h>
  
@@ -6,10 +8,10 @@ void matmulA(double **a, double **b, double **c, int a_row, int common, int b_co
 {
     for (int i = 0; i < a_row; i++)
     {
-        for (int j = 0; j < common; j++)
+        for (int j = 0; j < b_col; j++)
         {
             double sum = 0;
-            for (int k = 0; k < b_col; k++)
+            for (int k = 0; k < common; k++)
             {
                 sum += a[i][k] * b[k][j];
             }
@@ -18,57 +20,64 @@ void matmulA(double **a, double **b, double **c, int a_row, int common, int b_co
     }
 }
  
-void matmulB(double **a, double **b, double **c, int a_row, int common, int b_col)
+void matmulB(double ** a, double ** b, double ** c, int a_row, int common, int b_col)
 {
-    for (int i = 0; i != a_row; ++i)
+    for (int i = 0; i < a_row; i++)
     {
-        for (int j = 0; j != common; ++j)
+        for (int j = 0; j < b_col; j+=2)
         {
             double sum = 0;
-            for (int k = 0; k < b_col; k++)
+            for (int k = 0; k < common; k++)
             {
                 sum += a[i][k] * b[k][j];
             }
             c[i][j] = sum;
+
+            sum = 0;
+            for (int k = common-1; k >=0; k--)
+            {
+                sum += a[i][k] * b[k][j+1];
+            }
+            c[i][j+1] = sum;
         }
     }
+}
+
+double ** g_to_d(gsl_matrix* matrix)
+{
+    double ** retval = malloc(sizeof(double*)*matrix->size1);
+    for(int i = 0; i < matrix->size1; i++)
+    {
+        retval[i] = (double*)&matrix->data[i*matrix->tda];
+    }
+    return retval;
 }
  
 int main(int argsc, char** argv)
 {
-    int a_row = 20;
-    int common = 40;
-    int b_col = 40;
+    int a_row = 200;
+    int common = 400;
+    int b_col = 300;
 
-    double ** a = malloc(sizeof(double*)*a_row);
-    for(int i = 0; i < a_row; i++)
-    {
-        a[i] = malloc(sizeof(double)*common);
-    }
+    gsl_matrix * a = gsl_matrix_alloc(a_row,common);
+    double ** dA = g_to_d(a);
 
-    double ** b = malloc(sizeof(double*)*common);
-    for(int i = 0; i < common; i++)
-    {
-        b[i] = malloc(sizeof(double)*b_col);
-    }
+    gsl_matrix * b = gsl_matrix_alloc(common,b_col);
+    double ** dB = g_to_d(b);
 
-    double ** c = malloc(sizeof(double*)*a_row);
-    for(int i = 0; i < a_row; i++)
-    {
-        c[i] = malloc(sizeof(double)*b_col);
-    }
-        
+    gsl_matrix * c = gsl_matrix_alloc(a_row,b_col);        
+    double ** dC = g_to_d(c);
 
     clock_t start = clock();
     for(int i = 0; i < 100; i++)
         if(argv[1][0] == 'A')
-            matmulA(a,b,c,a_row,common,b_col);
-        else
-            matmulB(a,b,c,a_row,common,b_col);
+            matmulA(dA,dB,dC,a_row,common,b_col);
+        else if(argv[1][0] == 'B')
+            matmulB(dA,dB,dC,a_row,common,b_col);
+        else if(argv[1][0] == 'C')
+            gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, a, b, 0.0, c);
 
     printf("Time for consumer %lu\n", clock()-start);
 
-
-    
 }
 
